e0c996dd700476493e0b0fe54044fd14ef8bf46d
Bryan Alejandro Cubillos Prieto <bryan.cubillos@bizagi.com>
2025-03-23 11:07:15 -0500

    UI:MOB: Start band names

---
+++ lib/services/socket.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+enum ServerStatus {
+  online,
+  offline,
+  connecting
+}
+
+class SocketService with ChangeNotifier {
+
+  ServerStatus _serverStatus = ServerStatus.connecting;
+
+  get serverStatus => _serverStatus;
+
+  SocketService() {
+    _initConfig();
+  }
+
+  void _initConfig() {
+
+    String serverUrl = Platform.isIOS ? '127.0.0.1' : '10.0.2.2';
+
+    // Dart client
+    IO.Socket socket = IO.io('http://${serverUrl}:3000/', {
+      'transports': ['websocket'],
+      'autoConnect': true,
+      'forceNew': true,
+      'reconnect': true,
+    });
+
+    socket.onConnect((_) {
+      _serverStatus = ServerStatus.online;
+      notifyListeners();
+    });
+
+    socket.onDisconnect((_) {
+      _serverStatus = ServerStatus.offline;
+      notifyListeners();
+    });
+
+    socket.onConnectError((data) {
+      if(kDebugMode) {
+        print('Connect Error: $data');
+      }
+    });
+
+    socket.onError((data) {
+      if(kDebugMode) {
+        print('Error: $data');
+      }
+    });
+  }
+  
+}
